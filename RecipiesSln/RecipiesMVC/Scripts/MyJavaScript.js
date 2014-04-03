f841d069825f7fb29c506c72ec4b8b32608b54d7
y-todorov <ytodorov@ytodorov.com>
2014-04-04 00:18:36 +0300

    clean layout from js

--- RecipiesSln/RecipiesMVC/Scripts/MyJavaScript.js
+++ RecipiesSln/RecipiesMVC/Scripts/MyJavaScript.js
@@ -30,3 +30,46 @@ function OnRequestStart() {
 function OnResponseEnd() {
     isInRequest = false;
 }
+
+function exportGridData(sender) {
+
+    var gridDiv = ($(sender)).parents('div[class~="k-grid"]').first(); // да точно така е!!!
+    var grid = gridDiv.data("kendoGrid");
+
+    $.ajax({
+        type: "POST",
+        url: "/Download/ExportWithOpenXML",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify({
+            html: grid.table.context.innerHTML
+        }),
+        success: function (data) {
+        },
+        error: function (result) {
+            alert('Oh no: ' + result.responseText);
+        },
+        async: false
+    });
+}
+
+function getWeekString(weekInt) {
+    //debugger;
+    var res;
+    $.ajax({
+        type: "POST",
+        url: "/Chart/GetWeekString",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify({
+            dateEncoded: weekInt
+        }),
+        success: function (data) {
+            res = data;
+        },
+        error: function (result) {
+            alert('Oh no: ' + result.responseText);
+        },
+        async: false
+    });
+
+    return res;
+}
