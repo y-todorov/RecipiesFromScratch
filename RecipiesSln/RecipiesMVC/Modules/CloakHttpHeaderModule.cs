422f9b0ec0e7a156cc02e79adf46bd665e663ee8
y-todorov <ytodorov@ytodorov.com>
2014-10-05 01:05:53 +0300

    try to optimize the site

---
+++ RecipiesSln/RecipiesMVC/Modules/CloakHttpHeaderModule.cs
@@ -0,0 +1,69 @@
+﻿// http://consultingblogs.emc.com/howardvanrooijen/archive/2009/08/25/cloaking-your-asp-net-mvc-web-application-on-iis-7.aspx
+namespace RecipiesMVC.Modules
+{
+    #region Using Directives
+
+    using System;
+    using System.Collections.Generic;
+    using System.Web;
+
+    #endregion
+
+    /// <summary>
+    /// Custom HTTP Module for Cloaking IIS7 Server Settings to allow anonymity
+    /// </summary>
+    public class CloakHttpHeaderModule : IHttpModule
+    {
+        /// <summary>
+        /// List of Headers to remove
+        /// </summary>
+        private List<string> headersToCloak;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CloakHttpHeaderModule"/> class.
+        /// </summary>
+        public CloakHttpHeaderModule()
+        {
+            this.headersToCloak = new List<string>
+                                      {
+                                              "E-Tag",
+                                              "Server",
+                                              "X-AspNet-Version",
+                                              "X-AspNetMvc-Version",
+                                              "X-Powered-By",
+                                      };
+        }
+
+        /// <summary>
+        /// Dispose the Custom HttpModule.
+        /// </summary>
+        public void Dispose()
+        {
+        }
+
+        /// <summary>
+        /// Handles the current request.
+        /// </summary>
+        /// <param name="context">
+        /// The HttpApplication context.
+        /// </param>
+        public void Init(HttpApplication context)
+        {
+            context.PreSendRequestHeaders += this.OnPreSendRequestHeaders;
+        }
+
+        /// <summary>
+        /// Remove all headers from the HTTP Response.
+        /// </summary>
+        /// <param name="sender">
+        /// The object raising the event
+        /// </param>
+        /// <param name="e">
+        /// The event data.
+        /// </param>
+        private void OnPreSendRequestHeaders(object sender, EventArgs e)
+        {
+            this.headersToCloak.ForEach(h => HttpContext.Current.Response.Headers.Remove(h));
+        }
+    }
+}
