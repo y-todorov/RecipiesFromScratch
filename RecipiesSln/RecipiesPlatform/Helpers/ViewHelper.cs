6125a03357c66bb6dc8e0e7fcfc99bc69a259276
y-todorov <ytodorov@ytodorov.com>
2014-10-16 22:09:08 +0300

    force build

--- RecipiesSln/RecipiesPlatform/Helpers/ViewHelper.cs
+++ RecipiesSln/RecipiesPlatform/Helpers/ViewHelper.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace RecipiesMVC.Helpers
+﻿namespace RecipiesMVC.Helpers
 {
     public static class ViewHelper
     {
         public static string GetDateStringFromWeekNumber(int weekNumber)
         {
-            return "";
+            return ""; //f
         }
     }
 }
